ccec9e09a7882486df088450cf277fd4b74f7728
chinmaysawaji <chinmaysawaji@gmail.com>
2014-12-10 06:52:59 +0530

    Adding 1- routes - GET APIs 2- /author/:author gets list of book information given an author 3- /title/:title gets book information given a title
    A bit of logging.

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,47 @@
+/**
+ * Handles requests for /book
+ */
+
 var express = require('express');
 var app = module.exports = express();
-var Book = require('../model/book');
+var Book = require('../model/book'); // Fix This: Should be available globally
 
+/*
+ * Handles POST request for adding new book information.
+ */
 app.post('/', function(req, res) {
+    console.log('Creating a new book...');
+
     var book = new Book();
     book.title = req.body.title;
     console.log('Title : ' + book.title);
     book.author = req.body.author;
     book.description = req.body.description;
+
+    console.log('Book { title : ' + book.title + ', author : ' + book.author + ', description : ' + book.description + ' }');
+    res.json(book);
+});
+
+/*
+ * Handles GET request for /book/title 
+ * Returns book information when a title is provided.
+ */
+app.get('/title/:title', function(req, res) {
+    var title = req.params.title;
+    console.log('Getting book information for title : ' + title);
+    var book = new Book();
+    book.title = title;
+    res.json(book);
+});
+
+/*
+ * Handles GET request for /book/author
+ * Returns a list of book information when an author is provided.
+ */
+app.get('/author/:author', function(req, res) {
+    var author = req.params.author;
+    console.log('Getting book information for author : ' + author);
+    var book = new Book();
+    book.author = author;
     res.json(book);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 /**
- *   Loads all the specified routes.
+ * Loads all the specified routes.
  */
 var routes = require('node-require-directory')(__dirname);
 
 module.exports = function(app) {
     Object.keys(routes).forEach(function(key) {
         if (key === 'index') return;
+        console.log('Registering /' + key + ' route');
 	app.use('/' + key, routes[key]);
     });
+    console.log('Route registration completed ...');
 };
